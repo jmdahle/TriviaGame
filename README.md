ebe0f1acfded9c3bad88524848d8f9ce1efc6952
JMDahle <47642064+jmdahle@users.noreply.github.com>
2019-05-25 11:39:58 -0500

    added bg image and opacity of game area

--- README.md
+++ README.md
@@ -1,11 +1,8 @@
 # TriviaGame
 
 ## Instructions
+Easy-peasy.  Just read the questions and click on the correct answer.  Just make sure you do it before time runs out!
 
 ## Technical Notes
-
-## Credits
-
-## ToDo
-- [ ] Open item
-- [x] Closed item
+* Game randomizes from "trivia card" objects and randomizes the presentation order of the possible answers.
+* Game responds with green (correct) or red (incorrect or timed out) notice.
--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -66,7 +66,7 @@ function timerUpdate() {
     } else {
         $("#timeRemain").attr("class", "notCritical");
     }
-    $("#timeRemain").text(timeString);
+    $("#timeRemain").text("Time Remaining: " + timeString);
 }
 
 function timerInit(sec) {
@@ -146,8 +146,8 @@ function gameStart() {
     timerIsStarted = false; // reset the flag
 
     // clear the welcome message and hide
-    $("#gameMessage").html("");
-    $("#gameMessage").hide();
+    $("#gameMessage").html("<h4>Chicago Trivia!</h4>");
+    // $("#gameMessage").hide();
     //un hide Question, Result, and Answer area
     $("#gameQuestion").show();
     $("#gameResult").show();
